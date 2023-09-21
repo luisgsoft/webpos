ae292cf54a7f0333353e60f4bdbc35deee542116
lsalinas <luis@gsoft.es>
2023-09-21 10:10:27 +0200

    Fix magento invoice grid

--- Model/ResourceModel/Invoice/Grid/Collection.php
+++ Model/ResourceModel/Invoice/Grid/Collection.php
@@ -1,7 +1,15 @@
 <?php
+
 namespace Gsoft\Webpos\Model\ResourceModel\Invoice\Grid;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult as OriginalCollection;
+use Magento\Sales\Model\ResourceModel\Order\Invoice;
+use Psr\Log\LoggerInterface as Logger;
 
 
 /**
@@ -9,6 +17,21 @@ use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult as Orig
  */
 class Collection extends OriginalCollection
 {
+    public function __construct(
+        EntityFactory     $entityFactory,
+        Logger            $logger,
+        FetchStrategy     $fetchStrategy,
+        EventManager      $eventManager,
+                          $mainTable = 'sales_invoice_grid',
+                          $resourceModel = Invoice::class,
+        TimezoneInterface $timeZone = null
+    )
+    {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
+        $this->timeZone = $timeZone ?: ObjectManager::getInstance()
+            ->get(TimezoneInterface::class);
+    }
+
     protected function _renderFiltersBefore()
     {
 
